35d45896fee6115ff0e620c5d08a584a298e6731
amandsol <oliveira.amands96@gmail.com>
2022-08-18 17:34:37 -0300

    script to call manduca function

---
+++ R/01_manduca.R
@@ -0,0 +1,12 @@
+#Load packages
+library(MASS)
+library(dplyr)
+library(reshape2)
+library(tidyr)
+library(plyr)
+
+#Executing the function
+source("fct/manduca.R")
+
+# Generating the simulations
+manduca(1,1,100)
