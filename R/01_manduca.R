5d4c142d249ff6dff7c4e5fc33db18cc96533f5d
amandsol <oliveira.amands96@gmail.com>
2022-08-19 16:16:06 -0300

    adding the function of the covariance matrix

--- R/01_manduca.R
+++ R/01_manduca.R
@@ -1,16 +1,20 @@
+# Scientific programming course ICTP-Serrapilheira
+# First version: August 18th 2022
+# Run the manduca simulations
+
 #Load packages
 library(MASS)
 library(dplyr)
-library(reshape2)
 library(tidyr)
 library(plyr)
 
-#Executing the function
+#Executing the functions
 source("fct/manduca.R")
+source("fct/cor2cov.R")
 
 # Generating the simulations
 manduca(1, 0.1)
 manduca(1, 0.5)
 manduca(1, 1)
 
-#.....................................
+#....................................
